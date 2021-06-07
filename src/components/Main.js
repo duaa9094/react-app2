19f24ed1f27dbe41557d7245c4dd20d4cc3986d4
Duaa abudhaim <duaa.abudhaim@gmail.com>
2021-06-07 23:11:33 +0300

    final

---
+++ src/components/Main.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Hornedbeast from './Hornedbeast';
+
+
+class Main extends React.Component{
+    render(){
+        return(
+<div className='container2'>
+<Hornedbeast title='UniWhal'  description='A unicorn and a narwhal nuzzling their horns' imageurl='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'/>
+<Hornedbeast title='Rhino Family'  description='Mother (or father) rhino with two babies' imageurl='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'/>
+
+<Hornedbeast title='Unicorn'   description='Someone wearing a creepy unicorn head mask' imageurl='https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'/>
+<Hornedbeast title='UniLego'  description='Lego figurine dressed in a unicorn outfit' imageurl='https://images.unsplash.com/photo-1518946222227-364f22132616?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4836a6fca62e7dce9324346bacfde085&auto=format&fit=crop&w=2534&q=80'/>
+
+<Hornedbeast title='Basically a unicorn'  description='A narwhal is basically a unicorn after all, right?' imageurl='https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg'/>
+<Hornedbeast title='#truth'  description='The truth behind narwhals' imageurl='https://i.pinimg.com/originals/16/cf/2a/16cf2a0b3fd51b9bee08bb6296193b75.jpg'/>
+
+<Hornedbeast title='Baby Rhino'  description='This is actually a figurine but it looks kinda real' imageurl='https://secure.img1-ag.wfcdn.com/im/17007094/resize-h800%5Ecompr-r85/3589/35892451/Baby+Rhino+Figurine.jpg'/>
+<Hornedbeast title='Cera'  description='Three horns but still, horns. And who doesnt like The Land Before Time' imageurl='https://vignette.wikia.nocookie.net/landbeforetime/images/c/c3/Cera_infobox.png/revision/latest?cb=20180422005003'/>
+
+<Hornedbeast title='Narwhal costume'  description='A woman wearing a blue narwhal costume' imageurl='https://ae01.alicdn.com/kf/HTB18GwSQVXXXXaZaXXXq6xXFXXXh/Animal-Cosplay-Costume-Narwhal-Onesie-Mens-Womens-Cartoon-Whale-Pajamas.jpg'/>
+<Hornedbeast title='Rhino costume'  description='Mascots have to get their costumes somewhere' imageurl='https://www.shopmascot.com/image/cache/mascotnew/new196-800x800.jpg'/>
+
+<Hornedbeast title='Believe'  description='I believe in unicorns, do you?' imageurl='https://www.tinselbox.com/wp-content/uploads/2018/03/I-BELIEVE-IN-UNICORNS-FREE-PRINTABLE-WATERCOLOR-7-of-1.jpg'/>
+<Hornedbeast title='Markhor'  description='These wild goats eat snakes, then secrete a foam that locals fight over for the antivemon properties -- how cool is that' imageurl='https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg/220px-Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg'/>
+
+<Hornedbeast title='Baby markhor'  description='Even the babies are adorable' imageurl='http://www.zooborns.com/.a/6a010535647bf3970b0223c84d5959200c-800wi'/>
+<Hornedbeast title='Mouflon'  description='Those horns though' imageurl='https://images.unsplash.com/photo-1558560063-931ca9822a0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'/>
+
+<Hornedbeast title='Addax'  description='This guy is basically extinct but survives well in captivity, so they are frequently found in zoos' imageurl='https://images.unsplash.com/photo-1556890077-020ec300d5db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80'/>
+<Hornedbeast title='Baby mouflen'  description='The cuteness that is a baby mouflon asleep' imageurl='https://cbsnews3.cbsistatic.com/hub/i/r/2013/03/05/5b414225-a645-11e2-a3f0-029118418759/thumbnail/620x350/2d4cf24685b45c22912e64d2004fec8d/Baby_Mouflon_Wild_Sheep.jpg'/>
+
+<Hornedbeast title='Happy Jackson Chameleon'  description='These are really common in Hawaii' imageurl='https://images.unsplash.com/photo-1514036783265-fba9577fc473?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'/>
+<Hornedbeast title='Serious Jackson Chameleon'  description='This one is very serious.' imageurl='https://imgc.allpostersimages.com/img/print/posters/dlillc-jackson-s-chameleon_a-G-13448768-14258384.jpg'/>
+ 
+
+<Hornedbeast title='Horned Lizard'  description='Fave food: ants' imageurl='https://www.nps.gov/band/learn/nature/images/short-horned-lizard-open-mouth-18.jpg?maxwidth=650&autorotate=false'/>
+<Hornedbeast title='Smaug'  description='Fan illustration of Smaug from The Hobbit' imageurl='https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/290px-Smaug_par_David_Demaret.jpg'/>
+
+
+</div>
+        )
+    }
+}
+export default Main;
